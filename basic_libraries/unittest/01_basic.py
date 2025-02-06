a70b72d8dc8ce875d1397caca57b93b31c75fb19
Devendra Agarwal <a.devendra@live.in>
2025-02-06 16:13:14 +0530

    Add UnitTest Case

--- basic_libraries/unittest/01_basic.py
+++ basic_libraries/unittest/01_basic.py
@@ -3,6 +3,11 @@ import unittest
 def add(a, b):
     return a + b
 
+def divide(a, b):
+    if b == 0:
+        raise ValueError("Divide By Zero Not Valid")
+    return round(a/b, 2)
+
 class TestAddFunction(unittest.TestCase):
     def test_add_positive_numbers(self):
         self.assertEqual(add(1, 2), 3)
@@ -13,6 +18,18 @@ class TestAddFunction(unittest.TestCase):
     def test_add_mixed_numbers(self):
         self.assertEqual(add(1, -2), -1)
         self.assertEqual(add(-1, 2), 1)
+
+    def test_divide_function(self):
+        self.assertEqual(divide(10, 3), 3.33)
+        self.assertEqual(divide(10, 5), 2)
+        self.assertEqual(divide(-10, 5), -2)
+        self.assertEqual(divide(10, -5), -2)
+        self.assertEqual(divide(-10, -5), 2)
+
+    def test_divide_with_exception(self):
+        
+        with self.assertRaises(ValueError):
+            divide(10, 0)
 
 if __name__ == '__main__':
     unittest.main()
